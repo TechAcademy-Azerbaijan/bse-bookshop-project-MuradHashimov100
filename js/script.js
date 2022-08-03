15f0d4ff8726af85d39b95ccd8c27b8590c27f0d
MuradHashimov100 <90892801+MuradHashimov100@users.noreply.github.com>
2022-08-03 23:31:41 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-const openMenu = document.querySelector('header .openmenu');
-const menyu = document.querySelector('.qasqabaq');
-openMenu.addEventListener("click", function(){
-    menyu.classList.toggle("active");
-    console.log('btn clicked');
-});
+
